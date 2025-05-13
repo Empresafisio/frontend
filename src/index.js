19015406fed602531e89d24eb720eaf3cf64ac8b
leomrosa <leo.m.rosa@gmail.com>
2025-05-13 21:16:40 +0100

    Autentication

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -13,6 +13,7 @@ import Register from "./pages/Register.jsx";
 import EditClientProfile from "./pages/EditClientProfile.jsx";
 import PhysioDashboard from "./pages/PhysioDashboard.jsx";
 import { AuthProvider } from "./components/context/AuthContext.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx"; // novo componente
 
 ReactDOM.render(
   <React.StrictMode>
@@ -25,14 +26,15 @@ ReactDOM.render(
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/contact-us" element={<ContactUs />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} /> 
-          <Route path="/cliente" element={<ClientDashboard />} />
-          <Route path="/cliente/editar-perfil" element={<EditClientProfile />} />
-          <Route path="/fisioterapeuta" element={<PhysioDashboard />} /> 
+          <Route path="/register" element={<Register />} />
+          
+          {/* Rotas protegidas */}
+          <Route path="/cliente" element={<PrivateRoute><ClientDashboard /></PrivateRoute>} />
+          <Route path="/cliente/editar-perfil" element={<PrivateRoute><EditClientProfile /></PrivateRoute>} />
+          <Route path="/fisioterapeuta" element={<PrivateRoute><PhysioDashboard /></PrivateRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
